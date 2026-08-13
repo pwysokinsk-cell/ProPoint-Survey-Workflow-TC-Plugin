96226a96fc20a781b7277b50dfa1022cb9b29aac
pwysokinsk-cell <p.wysokinsk@gmail.com>
2026-08-13 09:17:05 +0200

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ProPoint-Survey-Workflow-TC-Plugin/',
   plugins: [react()],
 });
